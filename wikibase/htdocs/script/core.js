ae7d0159ea4e3368d6f5aed0f1af21c24118c6aa
FizzFizz <fizz.from.outerspac@gmail.com>
2021-05-29 10:28:52 +0900

    master

---
+++ wikibase/htdocs/script/core.js
@@ -0,0 +1,24 @@
+﻿function setAddButtonEvent(){
+  const target = document.getElementById('add_section');
+  let count = 0;
+  target.addEventListener('click', () => {
+    alert('test');
+  }, false);
+}
+
+function loadHTMLByID(_html,id){
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET",_html,true);
+  xmlhttp.onreadystatechange = function(){
+    if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      var data = xmlhttp.responseText;
+      var elem = document.getElementById(id);
+      elem.innerHTML = data;
+      return data;
+    }
+  }
+  xmlhttp.send(null);
+}
+
+loadHTMLByID('html/footer.html','footer');
+setAddButtonEvent();
---
+++ wikibase/htdocs/script/inline_watch.js
@@ -0,0 +1,13 @@
+﻿/*time*/
+function InnlineWatch(){
+  div = document.getElementById("t1")
+  div.innerHTML=getTimeString();
+  setInterval(function(){
+    div.innerHTML = getTimeString();
+  },1000);
+}
+
+function getTimeString(){
+  var dd = new Date();
+  return dd.toLocaleString();
+}
