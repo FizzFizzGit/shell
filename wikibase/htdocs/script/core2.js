261fc54cde8e7c960286c0e982025f035a876240
FizzFizz <fizz.from.outerspac@gmail.com>
2021-05-29 10:20:23 +0900

    master

---
+++ wikibase/htdocs/script/core2.js
@@ -0,0 +1,15 @@
+﻿function loadHTMLByID(_html,id){
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET",_html,true);
+  xmlhttp.onreadystatechange = function(){
+    if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      var data = xmlhttp.responseText;
+      var elem = document.getElementById(id);
+      elem.innerHTML = data;
+      return data;
+    }
+  }
+  xmlhttp.send(null);
+}
+
+loadHTMLByID('../html/footer.html','footer');
